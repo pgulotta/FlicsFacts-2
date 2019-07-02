2d63872264f84789165d1d7586c372e9c79e25fb
Pat Gulotta <pgulotta@gmail.com>
2019-07-02 18:02:00 -0500

    updates for 64 bit support

--- FlicsFacts/main.cpp
+++ FlicsFacts/main.cpp
@@ -8,7 +8,7 @@ int main( int argc, char* argv[] )
   QApplication::setApplicationName( "Flics Facts" );
   QApplication::setOrganizationDomain( "twentysixapps.com" );
   QCoreApplication::setOrganizationName( QLatin1String( "26Apps" ) );
-  QApplication::setApplicationVersion( "2.30" );
+  QApplication::setApplicationVersion( "2.35" );
   QQuickStyle::setStyle( "Material" );
   QApplication app( argc, argv );
   QObject::connect( &app, &QApplication::lastWindowClosed, &app, &QApplication::quit );
